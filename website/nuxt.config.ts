a46eac51d7539c0ef0294a29a51ea971bd10b4ae
michalfedyna <michal@fedyna.pl>
2024-03-02 23:20:20 +0100

    add monaco-editor

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxt-monaco-editor"],
+  ssr: false,
   css: ["~/assets/css/main.css"],
   typescript: {
     typeCheck: true,
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,3 +1,20 @@
+<style>
+.monaco-editor {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
+
 <template>
-  <h1>Hello Nuxt!</h1>
+  <MonacoEditor :options="options" v-model="value" lang="typescript" class="monaco-editor" />
 </template>
+
+<script setup lang="ts">
+import type * as Monaco from "monaco-editor";
+
+const options = ref<Monaco.editor.IStandaloneEditorConstructionOptions>({
+  theme: "vs-dark",
+  minimap: { enabled: false },
+});
+const value = ref("console.log('Hello, World!')");
+</script>
